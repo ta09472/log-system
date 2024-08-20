898c247481ccddf773c3051b62d7e820f8c8d90a
beomsuKim <ta09472@naver.com>
2024-08-20 16:38:54 +0900

    feat: 비동기 통신 에러 핸들링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import "antd/dist/reset.css";
 import "./index.css";
 import Dashboard from "./page/Dashboard";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, message } from "antd";
 import Results from "./page/Results";
 import NotFound from "./page/NotFound";
 import ErrorBoundary from "./components/ErrorBoundary";
 import AggregationResult from "./components/AggregationResult";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: error => {
+      message.error(error.message);
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: error => {
+      message.error(error.message);
+    },
+  }),
+});
 
 const App = () => (
   <ErrorBoundary>
